ecd816241b5785d7110c1e584e41fead5e1708e5
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-06 06:30:05 -0800

    add task12 send email

---
+++ task12/src/App.js
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+function App() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6z075zp', 'template_8fmcvbp', form.current, 'BavgMCQbnVmtb0apW')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
+
+export default App;
